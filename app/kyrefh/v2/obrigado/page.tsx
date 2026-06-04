0a7ee89049fa53983f0c5570a893bb280d97cb8f
duacriativa <suporte@duacriativa.com>
2026-06-04 16:54:34 -0300

    Fix ESLint: escapar aspas no obrigado/page.tsx (react/no-unescaped-entities)

--- app/kyrefh/v2/obrigado/page.tsx
+++ app/kyrefh/v2/obrigado/page.tsx
@@ -118,7 +118,7 @@ export default function KyrefhV2ObrigadoPage() {
               fontStyle: 'italic',
             }}
           >
-            "{MESSAGE}"
+            &ldquo;{MESSAGE}&rdquo;
           </p>
         </div>
 
